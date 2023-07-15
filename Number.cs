24223f9ad3ace0056262ebc4661df3aa15f1067f
Dwiky <hanafihanatapraja@students.amikom.ac.id>
2023-07-15 15:38:38 +0700

    first commit

---
+++ Number.cs
@@ -0,0 +1,100 @@
+﻿using System;
+namespace Pertemuan_5
+{
+	public class Number
+	{
+        float min;
+        int min2;
+
+        float max;
+        int max2;
+
+        public int findMinimum(int number1, int number2)
+        {
+            int[] values = { number1, number2 };
+            for (int i = 0; i < 2; i++)
+            {
+                if (i == 0)
+                {
+                    min2 = values[i];
+
+                }
+                else if (values[i] < min2)
+                {
+                    min2 = values[i];
+
+                }
+
+            }
+            return min2;
+
+        }
+
+        public int findMinimum(int number1, int number2, float number3)
+        { 
+            float[] values = {number1, number2, number3};
+            for(int i = 0; i < 3; i++)
+            {
+
+
+                if (i == 0)
+                {
+                    min = values[i];
+
+                }
+                else if (values[i] < min)
+                {
+                    min = values[i];
+                }
+            }
+            return (int)min;
+        }
+
+
+
+        public int findMaximum(int number1, int number2)
+        {
+            int[] values = { number1, number2 };
+            for (int i = 0; i < 2; i++)
+            {
+                if (i == 0)
+                {
+                    max2 = values[i];
+
+                }
+                else if (values[i] > min2)
+                {
+                    max2 = values[i];
+
+                }
+
+            }
+            return max2;
+
+        }
+
+
+        public int findMaximum(int number1, int number2, float number3)
+        {
+            float[] values = { number1, number2, number3 };
+            for (int i = 0; i < 3; i++)
+            {
+
+
+                if (i == 0)
+                {
+                    max = values[i];
+
+                }
+                else if (values[i] > min)
+                {
+                    max = values[i];
+                }
+            }
+            return (int)max;
+        }
+
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Pertemuan_5;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Number number = new Number();
+
+        Console.WriteLine($"Minimum #1 : {number.findMinimum(3, 1)}");
+        Console.WriteLine($"Minimum #2 : {number.findMinimum(3, 2, 4)}");
+        Console.WriteLine();
+        Console.WriteLine($"Maximum #3 : {number.findMaximum(3, 1)}");
+        Console.WriteLine($"Maximum #4 : {number.findMaximum(3, 2, 4)}");
+
+        Console.ReadKey();
+    }
+}
+
